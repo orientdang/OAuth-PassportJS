aae3bf22c59782290c4fae1523d830dbd5c207b9
Dang Nguyen Hai <dang.nguyen@orientsoftware.com>
2019-05-10 11:31:33 +0700

    add User Model Schema

--- model/userModel.js
+++ model/userModel.js
@@ -1,6 +1,5 @@
 const mongoose = require("mongoose");
-const Schema = mongoose.Schema;
-
+const bcrypt = require("bcrypt");
 const userSchema = new mongoose.Schema({
     userName: {
         type: String
@@ -12,5 +11,18 @@ const userSchema = new mongoose.Schema({
         type: String
     }
 });
+// pre save()
+userSchema.pre("save", async next => {
+    const user = this;
+    const hash = await bcrypt.hash(this.password, 10);
+    this.password = hash;
+    next();
+});
+// valid user'password
+userSchema.method.isPasswordValid = async function(next) {
+    const user = this;
+    const compare = await bcrypt.compare(password, user.password);
+    return compare;
+};
 
 module.exports = mongoose.model("User", userSchema);
